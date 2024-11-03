0c3efd0e37145b29a5a12228f2464aa224d56bb5
AleDeb93 <alex.debiasi@email.com>
2024-11-03 11:03:04 +0100

    Home component and uploaded imgs

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  scrolled = false
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(){
+    this.scrolled = window.scrollY > 600;
+  }
+
 }
