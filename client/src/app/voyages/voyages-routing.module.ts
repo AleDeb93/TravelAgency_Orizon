8b58321463d39ee43ba9c3de0d5a5e0427e481a4
AleDeb93 <alex.debiasi@email.com>
2024-11-16 10:38:20 +0100

    LazyModule works!

--- client/src/app/voyages/voyages-routing.module.ts
+++ client/src/app/voyages/voyages-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { OffersComponent } from './offers/offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'destinations', component: DestinationsComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'offers', component: OffersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
