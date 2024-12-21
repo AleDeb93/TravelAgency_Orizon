10e6b2648138bf7f32a0d65432af1eea505f01af
AleDeb93 <alex.debiasi@email.com>
2024-12-21 11:23:16 +0100

    Destination page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DestinationsComponent } from './voyages/destinations/destinations.component';
 import { UserComponent } from './components/user/user.component';
@@ -11,12 +11,19 @@ const routes: Routes = [
     path: 'voyages',
     loadChildren: () => import('./voyages/voyages.module').then(m => m.VoyagesModule)
   },
-  { path: '', redirectTo: './voyages/destinations', pathMatch: 'full' },
-  { path: 'account', component: UserComponent }
+  { path: '', redirectTo: 'voyages/destinations', pathMatch: 'full' }
 ];
 
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  // scrollOffset: [0, 64],
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
