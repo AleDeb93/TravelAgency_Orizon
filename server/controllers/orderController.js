a914886cc29551e226e7e56431962334c11de2f6
AleDeb93 <121309726+AleDeb93@users.noreply.github.com>
2025-01-20 16:47:55 +0100

    Update orderController.js

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,4 +1,6 @@
 const Orders = require('../models/Orders');
+const Items = require('../models/Items');
+const Destinations = require('../models/Destinations');
 
 const orderController = {
     // GET /api/v2/orders
@@ -13,3 +15,40 @@ const orderController = {
 };
 
 module.exports = orderController;
+
+
+// // GET /api/v2/orders
+// getAllOrders: async (req, res) => {
+//     try {
+//         const orders = await Orders.findAll({
+//             include: [
+//                 {
+//                     model: Destinations, // includo destinazioni tramite la relazione many-to-many
+//                     as: 'destinations',
+//                     through: {
+//                         attributes: ['order', 'destination', 'buyedTickets'] // includo solo i dati di Items
+//                     }
+//                 }
+//             ]
+//         });
+
+//         // Modifica i dati degli ordini per includere il numero di biglietti per destinazione
+//         const result = orders.map(order => {
+//             return {
+//                 ...order.toJSON(),
+//                 destinations: order.destinations.map(destination => {
+//                     const item = destination.Items[0]; 
+//                     return {
+//                         ...destination,
+//                         buyedTickets: item.buyedTickets // numero di biglietti acquistati
+//                     };
+//                 })
+//             };
+//         });
+
+//         res.status(200).json(result);
+//     } catch (error) {
+//         res.status(500).json({ error: 'Errore durante la chiamata getAllOrders' });
+//     }
+// }
+// };
